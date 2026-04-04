76d750cf78cbadaa3703742e66b97da2f9e9180d
kushal sw <kushal@kushals-MacBook-Air.local>
2026-04-04 10:07:01 +0530

    new

--- binearyalgoo/binearyalgo.cpp
+++ binearyalgoo/binearyalgo.cpp
@@ -1,123 +1,157 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
 
-int binearysearch(vector<int> arr , int tar) {
-    int st = 0 , end = arr.size() - 1;
+// int binearysearch(vector<int> arr , int tar) {
+//     int st = 0 , end = arr.size() - 1;
     
     
-    while(st <= end){
-        int mid = ( st + end ) /2;
+//     while(st <= end){
+//         int mid = ( st + end ) /2;
 
-        if ( tar > arr[mid]){
-            st = mid + 1;
-        } else if (tar < arr[mid]){
-            st = mid - 1; 
-        }
-        else {
-            return mid;
+//         if ( tar > arr[mid]){
+//             st = mid + 1;
+//         } else if (tar < arr[mid]){
+//             st = mid - 1; 
+//         }
+//         else {
+//             return mid;
 
-        }
+//         }
         
 
      
 
-    } 
-    return -1;
-}
+//     } 
+//     return -1;
+// }
 
 
-int main() {
-    vector<int> arr1 = {-1,3,4,5,9,1,12};
-    int tar1 = 12;
-    cout << binearysearch( arr1 , tar1) << endl;
+// int main() {
+//     vector<int> arr1 = {-1,3,4,5,9,1,12};
+//     int tar1 = 12;
+//     cout << binearysearch( arr1 , tar1) << endl;
   
-    vector<int> arr2 = {-2,0,2,4,5,19};
-    int tar2 = 0;
-  cout << binearysearch( arr2 , tar2) << endl;
-    return 0;
+//     vector<int> arr2 = {-2,0,2,4,5,19};
+//     int tar2 = 0;
+//   cout << binearysearch( arr2 , tar2) << endl;
+//     return 0;
 
-}
-// binary search ---------------------------------------------------------------------------
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+// }
+// // binary search ---------------------------------------------------------------------------
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
 
-int binarySearch(vector<int> arr, int tar) {
-    int st = 0, end = arr.size() - 1;
+// int binarySearch(vector<int> arr, int tar) {
+//     int st = 0, end = arr.size() - 1;
 
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
+//     while (st <= end) {
+//         int mid = st + (end - st) / 2;
 
-        if (tar > arr[mid]) {
-            st = mid + 1;
-        } 
-        else if (tar < arr[mid]) {
-            end = mid - 1; // FIXED
-        } 
-        else {
-            return mid;
-        }
-    }
-    return -1;
-}
+//         if (tar > arr[mid]) {
+//             st = mid + 1;
+//         } 
+//         else if (tar < arr[mid]) {
+//             end = mid - 1; // FIXED
+//         } 
+//         else {
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
 
-int main() {
-    vector<int> arr1 = {-1,3,4,5,9,1,12};
-    sort(arr1.begin(), arr1.end());
+// int main() {
+//     vector<int> arr1 = {-1,3,4,5,9,1,12};
+//     sort(arr1.begin(), arr1.end());
 
-    vector<int> arr2 = {-2,0,2,4,5,19};
+//     vector<int> arr2 = {-2,0,2,4,5,19};
 
-    cout << binarySearch(arr1, 12) << endl;
-    cout << binarySearch(arr2, 0) << endl;
+//     cout << binarySearch(arr1, 12) << endl;
+//     cout << binarySearch(arr2, 0) << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
 
-int recbinarySearch(vector<int> arr, int tar ,int st , int end) {
-    if (st <= end) {
-        int mid = st +  (end - st )  /2;
+// int recbinarySearch(vector<int> arr, int tar ,int st , int end) {
+//     if (st <= end) {
+//         int mid = st +  (end - st )  /2;
 
-        if (tar > arr[mid]){
-            return recbinarySearch(arr , tar , mid + 1 ,end);
-        }else if (tar < arr[mid]){
-            return recbinarySearch(arr , tar , st, mid - 1);
-        } else {
-            return mid;
-        }
-    }
+//         if (tar > arr[mid]){
+//             return recbinarySearch(arr , tar , mid + 1 ,end);
+//         }else if (tar < arr[mid]){
+//             return recbinarySearch(arr , tar , st, mid - 1);
+//         } else {
+//             return mid;
+//         }
+//     }
  
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
+//     while (st <= end) {
+//         int mid = st + (end - st) / 2;
 
-        if (tar > arr[mid]) {
-            st = mid + 1;
-        } 
-        else if (tar < arr[mid]) {
-            end = mid - 1; // FIXED
-        } 
-        else {
-            return mid;
-        }
-    }
-    return -1;
-}
+//         if (tar > arr[mid]) {
+//             st = mid + 1;
+//         } 
+//         else if (tar < arr[mid]) {
+//             end = mid - 1; // FIXED
+//         } 
+//         else {
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main() {
+//   vector<int> arr1 = {-1,3,4,5,9,12};
+//    int tar1 = 12;
+//         cout << recbinarySearch( arr1 , tar1, 0, static_cast<int>(arr1.size()) - 1) << endl;
+  
+//     vector<int> arr2 = {-2,0,2,4,5,19};
+//      int tar2 = 0;
+//    cout << recbinarySearch( arr2 , tar2, 0, static_cast<int>(arr2.size()) - 1) << endl;
+//      return 0;
+//  }         
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-  vector<int> arr1 = {-1,3,4,5,9,12};
-   int tar1 = 12;
-        cout << recbinarySearch( arr1 , tar1, 0, static_cast<int>(arr1.size()) - 1) << endl;
-  
-    vector<int> arr2 = {-2,0,2,4,5,19};
-     int tar2 = 0;
-   cout << recbinarySearch( arr2 , tar2, 0, static_cast<int>(arr2.size()) - 1) << endl;
-     return 0;
- }         
+    vector<int> arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int target = 91;
+    
+    int low = 0;
+    int high = arr.size() - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (arr[mid] == target) {
+            result = mid;
+            break;
+        }
+        
+        if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    cout << "Binary Search Result Index: " << result << endl;
+    return 0;
+}
